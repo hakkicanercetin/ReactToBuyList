4a73ea19e82204b8319fddd2046c2f14fb461859
Hakkıcan ERÇETİN <hakkicanercetin@gmail.com>
2024-01-18 16:57:46 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,16 @@
-/* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
 import { nanoid } from "nanoid";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from "react-bootstrap";
-import { categories, shops } from "./data/data";
 import { MyTable } from "./Components/Table/MyTable";
 import "./style.css"
 import JSConfetti from "js-confetti";
-
-function handleButton(product,setProduct,productList,setProductList)
-{
-  setProductList((oldProductList)=>[...oldProductList,{...product,id:product.id}])
-  setProduct({
-    name: "",
-    shop: "",
-    category: "",
-    isBought: false,
-    id: nanoid(),
-  })
-}
-function handleInput(e, product, setProduct) {
-  setProduct({ ...product, name: e.target.value });
-}
-function handleShopSelect(e, product, setProduct) {
-  setProduct({ ...product, shop: e.target.value });
-}
-function handleCategorySelect(e, product, setProduct) {
-  setProduct({ ...product, category: e.target.value });
-}
-function isProductBought(id, productList, setProductList) {
-  setProductList((oldProductList) => {
-    const updatedProductList = oldProductList.map((product) => {
-      if (product.id === id) {
-        return { ...product, isBought: true };
-      }
-      return product;
-    });
-    return updatedProductList;
-  });
-}
-function deleteProduct(id,productList,setProductList)
-{
-  setProductList((oldProductList) => {
-    const newProductList = oldProductList.filter((product)=> product.id != id)
-    return newProductList
-  })
-}
+import { ProductInput } from "./Components/ProductInput/ProductInput";
+import { ShopSelect } from "./Components/ShopSelect/ShopSelect";
+import { CategorySelect } from "./Components/CategorySelect/CategorySelect";
+import { deleteProduct, isProductBought } from "./Functions/EventFunctions";
+import { handleButton, handleCategorySelect, handleInput, handleShopSelect } from "./Functions/HandlerFunctions";
+import { FilteredTable } from "./Components/FilteredTable/FilteredTable";
 function App() {
   const [productList,setProductList] = useState([])
   const [product, setProduct] = useState({
@@ -69,22 +34,13 @@ function App() {
   return (
     <>
     <div className="d-flex justify-content-center">
-      <input onChange={(e)=>handleInput(e,product,setProduct)} value={product.name}/>
-      <select onChange={(e)=>handleShopSelect(e,product,setProduct)} value={product.shop}>
-            <option>Market seçiniz</option>
-            {shops.map((shop)=>(
-                <option key={shop.id} value={shop.id}>{shop.name}</option>
-            ))}
-      </select>
-      <select onChange={(e)=>handleCategorySelect(e,product,setProduct)} value={product.category}>
-            <option>Kategori seçin</option>
-            {categories.map((category)=>(
-                <option key={category.id} value={category.id}>{category.name}</option>
-            ))}
-      </select>
+    <ProductInput handleInput={(e)=>handleInput(e,product,setProduct)} product={product} />
+    <ShopSelect handleShopSelect={(e)=>handleShopSelect(e,product,setProduct)} product={product} />
+    <CategorySelect handleCategorySelect={(e)=>handleCategorySelect(e,product,setProduct)} product={product}/>
       <Button onClick={()=>{handleButton(product,setProduct,productList,setProductList)}}>Ekle</Button>
     </div>
       <MyTable productList={productList} isProductBought={(id)=>{isProductBought(id,productList,setProductList)}} deleteProduct={(id)=>deleteProduct(id,productList,setProductList)}></MyTable>
+    <FilteredTable handleShopSelect={(e)=>handleShopSelect(e,product,setProduct)} product={product} productList={productList}></FilteredTable>
     </>
   )
 }
